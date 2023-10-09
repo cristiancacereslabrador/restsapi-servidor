7703d1d47f0eeb5b706113e79ea6a7ed981a1f4d
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2023-10-09 15:11:10 -0500

    correccion UPLOADS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose.Promise = global.Promise;
 
 // console.log("process.env.DATA_BASE", process.env.DATA_BASE);
 mongoose
-  // .connect("mongodb://127.0.0.1:27017/restapis", {
+  //.connect("mongodb://127.0.0.1:27017/restapis", {
   .connect(process.env.DATA_BASE, {
     useNewUrlParser: true,
   })
@@ -26,6 +26,28 @@ mongoose
 
 // Crear el servidor
 const app = express();
+//Carpeta pública
+app.use(express.static("uploads")); //ORIGINAL
+// Ruta para servir archivos estáticos desde la carpeta "uploads"
+// app.use("/uploads", express.static("uploads"));
+
+//*EXPERIMENTAL
+// Configurar cabeceras y cors
+// // app.use((req, res, next) => {
+// //   res.header("Access-Control-Allow-Origin", "*");
+
+// //   res.header(
+// //     "Access-Control-Allow-Headers",
+// //     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+// //   );
+
+// //   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+// //   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+
+// //   next();
+// // });
+//*EXPERIMENTAL
 
 //Habiliar bodyparser
 app.use(bodyParser.json());
@@ -33,11 +55,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Definir un dominio(s) para recibir las peticiones
 const whitelist = [process.env.FRONTEND_URL];
-console.log("whitelist:", whitelist);
+let repeated = false;
+
 const corsOption = {
   origin: (origin, callback) => {
-    console.log("origin", origin);
-    //Revisar si la petición viene de un servidor que esta en whitelist
+    if (!repeated) {
+      console.log("origin:", origin);
+      repeated = true;
+    }
+    //Revisar si la petición viene de un servidor que está en whitelist
     const existe = whitelist.some((dominio) => dominio === origin);
     if (existe) {
       callback(null, true);
@@ -47,14 +73,25 @@ const corsOption = {
   },
 };
 
+// // console.log("whitelist:", whitelist);
+// // const corsOption = {
+// //   origin: (origin, callback) => {
+// //     console.log("origin", origin);
+// //     //Revisar si la petición viene de un servidor que esta en whitelist
+// //     const existe = whitelist.some((dominio) => dominio === origin);
+// //     if (existe) {
+// //       callback(null, true);
+// //     } else {
+// //       callback(new Error("No permitido por CORS"));
+// //     }
+// //   },
+// // };
+
 //Habilitar CORS
 app.use(cors(corsOption));
 
 // Rutas de la app
 app.use("/", routes());
-
-//Carpeta pública
-app.use(express.static("uploads"));
 
 // Puerto
 const port = process.env.PORT || 4000;
